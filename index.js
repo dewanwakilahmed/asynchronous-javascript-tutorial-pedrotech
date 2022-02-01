e84ae95b5e6ff22c72256249d2487604c6a00b28
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-02-02 01:37:36 +0600

    Async Await

--- index.js
+++ index.js
@@ -7,39 +7,54 @@
 // var listOfMovies = fetch("url.com/movies"); // does not work instantly
 // console.log(listOfMovies); // will return nothing
 
-const event = new Promise((resolve, reject) => {
-  var name = "Tech";
-  if (name == "Pedro") {
-    resolve(name);
-  } else {
-    reject("Name was not Pedro, name was " + name);
-  }
-});
+// const event = new Promise((resolve, reject) => {
+//   var name = "Tech";
+//   if (name == "Pedro") {
+//     resolve(name);
+//   } else {
+//     reject("Name was not Pedro, name was " + name);
+//   }
+// });
 
-event
-  .then((name) => {
-    console.log(name);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => {
-    console.log("PROMISED FINISHED!");
-  });
+// event
+//   .then((name) => {
+//     console.log(name);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     console.log("PROMISED FINISHED!");
+//   });
 
 const axios = require("axios");
 
-const data = axios.get("https://cat-fact.herokuapp.com/facts");
+// const data = axios.get("https://cat-fact.herokuapp.com/facts");
 
-console.log(data); // Promise { <pending> }
+// console.log(data); // Promise { <pending> }
 
-data
-  .then((res) => {
-    console.log(res.data);
-  })
-  .catch((err) => {
+// data
+//   .then((res) => {
+//     console.log(res.data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     console.log("Promise resolved!");
+//   });
+
+// async function fetchData () {}
+
+const fetchData = async () => {
+  try {
+    const data = await axios.get("https://cat-fact.herokuapp.com/facts");
+    console.log(data.data);
+  } catch (err) {
     console.log(err);
-  })
-  .finally(() => {
-    console.log("Promise resolved!");
-  });
+  } finally {
+    console.log("FETCHDATA RESOLVED!");
+  }
+};
+
+fetchData();
