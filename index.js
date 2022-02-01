0b36ffa8d3db358bb018a066878a574f08b8b59d
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-02-02 01:38:46 +0600

    Uncommented

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-// var name = "Pedro";
-// console.log(name);
+var name = "Pedro";
+console.log(name);
 
-// name = "John";
-// console.log(name); // works instantly
+name = "John";
+console.log(name); // works instantly
 
-// var listOfMovies = fetch("url.com/movies"); // does not work instantly
-// console.log(listOfMovies); // will return nothing
+var listOfMovies = fetch("url.com/movies"); // does not work instantly
+console.log(listOfMovies); // will return nothing
 
-// const event = new Promise((resolve, reject) => {
-//   var name = "Tech";
-//   if (name == "Pedro") {
-//     resolve(name);
-//   } else {
-//     reject("Name was not Pedro, name was " + name);
-//   }
-// });
+const event = new Promise((resolve, reject) => {
+  var name = "Tech";
+  if (name == "Pedro") {
+    resolve(name);
+  } else {
+    reject("Name was not Pedro, name was " + name);
+  }
+});
 
-// event
-//   .then((name) => {
-//     console.log(name);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   })
-//   .finally(() => {
-//     console.log("PROMISED FINISHED!");
-//   });
+event
+  .then((name) => {
+    console.log(name);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log("PROMISED FINISHED!");
+  });
 
 const axios = require("axios");
 
-// const data = axios.get("https://cat-fact.herokuapp.com/facts");
+const data = axios.get("https://cat-fact.herokuapp.com/facts");
 
-// console.log(data); // Promise { <pending> }
+console.log(data); // Promise { <pending> }
 
-// data
-//   .then((res) => {
-//     console.log(res.data);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   })
-//   .finally(() => {
-//     console.log("Promise resolved!");
-//   });
+data
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log("Promise resolved!");
+  });
 
 // async function fetchData () {}
 
